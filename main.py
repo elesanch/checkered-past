f97a021831d0815b83be4595a431c89bc3011239
elesanch <116669574+elesanch@users.noreply.github.com>
2022-12-13 11:52:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -35,25 +35,25 @@ def main_menu():
         clock.tick(FPS)
         board = Board()
         menu_mouse_pos = pygame.mouse.get_pos()
-        
-        play_button = Button(button_face, pos=(400, 150), text_input = "PLAY", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
-        mode_button = Button(button_face, pos=(400, 300), text_input = "GAME MODE", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
-        options_button = Button(button_face, pos=(400, 450), text_input = "OPTIONS", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
-        exit_button = Button(button_face, pos=(400, 600), text_input = "EXIT", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
-        
+
+        play_button = Button(button_face, pos = (400, 150), text_input = "PLAY", font = pygame.font.SysFont("couriernew", 50), base_color = "black", hovering_color = "red")
+        mode_button = Button(button_face, pos = (400, 300), text_input = "GAME MODE", font = pygame.font.SysFont("couriernew", 50), base_color = "black", hovering_color = "red")
+        options_button = Button(button_face, pos = (400, 450), text_input = "OPTIONS", font = pygame.font.SysFont("couriernew", 50), base_color = "black", hovering_color = "red")
+        exit_button = Button(button_face, pos = (400, 600), text_input = "EXIT", font = pygame.font.SysFont("couriernew", 50), base_color = "black", hovering_color = "red")
+
         for selection in [play_button, mode_button, options_button, exit_button]:
             selection.color_change(menu_mouse_pos)
             selection.update(WIN)
-            
+
         if board.winner() != None: 
             print(board.winner())
-            
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 sys.exit()
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if play_button.user_input(menu_mouse_pos):
                     main()
@@ -67,42 +67,48 @@ def main_menu():
                     sys.exit()
 
         pygame.display.update()
-        
+
+
 def main():
-    pygame.display.set_caption ('Checkers - Game')
+    pygame.display.set_caption ('Press Space Bar for Main Menu')
     run = True 
     clock = pygame.time.Clock()
     game = Game(WIN)
     board = Board()
-    
+
     while run: 
         clock.tick(FPS)
         WIN.fill("black")
-        
+
         if board.winner() != None: 
             print(board.winner())
-            
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 sys.exit()
-            
+
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 pos = pygame.mouse.get_pos()
                 row, col = get_pos_mouse(pos)
                 game.select(row,col)
-        
+            
+            ## Back button to go to main menu
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE: 
+                    main_menu()
+                    
         game.update()
-    
+
     pygame.quit()
 
 def game_mode():
-    pygame.display.set_caption('Game Mode Selection')
+    pygame.display.set_caption('Press Space Bar for Main Menu')
     run = True 
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("couriernew", 50)
-    
+
     while run: 
         clock.tick(FPS)
         WIN.fill("black")
@@ -115,35 +121,38 @@ def game_mode():
                 run = False
                 pygame.quit()
                 sys.exit()
-        
-        ## Need to add a back button to go to main menu
-        #    if event.type == pygame.MOUSEBUTTONDOWN:
-        
+            
+            ## Back button to go to main menu
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE: 
+                    main_menu()
+
         pygame.display.update()
 
 def options():
-    pygame.display.set_caption('Adjust Game Settings')
+    pygame.display.set_caption('Press Space Bar for Main Menu')
     run = True 
     clock = pygame.time.Clock()
     font = pygame.font.SysFont("couriernew", 50)
-    
+    keys = pygame.key.get_pressed()
     while run: 
         clock.tick(FPS)
         WIN.fill("black")
         options_text = font.render("This Screen Isn't Finished", True, "white")
         options_rect = options_text.get_rect(center=(400, 400))
         WIN.blit(options_text, options_rect)
-            
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 sys.exit()
         
-        ## Need to add a back button to go to main menu
-        #    if event.type == pygame.MOUSEBUTTONDOWN:
-            
+        ## Back button to go to main menu
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE: 
+                    main_menu()
+                
         pygame.display.update()
 ##############################################################################
 main_menu()
-# main()
