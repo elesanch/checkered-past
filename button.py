c2deb26351e9f99d9cdb94b1d88babf3760b502f
kcritte1 <63515843+kcritte1@users.noreply.github.com>
2022-12-12 20:55:27 -0500

    Add files via upload

--- button.py
+++ button.py
@@ -8,34 +8,41 @@ import pygame
 import sys
 from constants import WIDTH, HEIGHT
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("PLAY")
-main_font = pygame.font.SysFont("couriernew", 50)
+# pygame.init()
+# screen = pygame.display.set_mode((WIDTH, HEIGHT))
+# pygame.display.set_caption("PLAY")
+# pygame.font.SysFont("couriernew", 50)
 
 class Button():
-    def __init__(self, image, x, y, text_input):
-        self.x = x
-        self.y = y
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.x = pos[0]
+        self.y = pos[1]
         self.image = image
-        self.rect = self.image.get_rect(center = (self.x, self.y))
+        self.font = font
         self.text_input = text_input
-        self.text = main_font.render(self.text_input, True, "white")
+        self.base_color= base_color
+        self.hovering_color = hovering_color
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center = (self.x, self.y))
         self.text_rect = self.text.get_rect(center = (self.x, self.y))
         
-    def update(self):
-        screen.blit(self.image, self.rect)
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
         screen.blit(self.text, self.text_rect)
             
     def user_input(self, pos):
         if pos[0] in range(self.rect.left, self.rect.right) and pos[1] in range(self.rect.top, self.rect.bottom):
-            print("BUTTON CLICKED")
-        
+            return True
+        return False
+    
     def color_change(self, pos):
         if pos[0] in range(self.rect.left, self.rect.right) and pos[1] in range(self.rect.top, self.rect.bottom):
-            self.text = main_font.render(self.text_input, True, "red")
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
         else:
-            self.text = main_font.render(self.text_input, True, "black")
+            self.text = self.font.render(self.text_input, True, self.base_color)
 
 ## The code below was ran to test the functionality of the class             
 # button_face = pygame.image.load("button.jpg")
--- main.py
+++ main.py
@@ -5,16 +5,16 @@ Created on Tue Dec  6 14:07:02 2022
 
 @author: elenasanchez
 """
-mport pygame 
+import pygame
+import sys 
 from constants import WIDTH, HEIGHT, SQUARE_SIZE
 from board import Board 
 from game import Game
-
+from button import Button
 
 FPS = 60
-
+pygame.init()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption ('Checkers - Game')
 
 def get_pos_mouse(position): 
     x, y = position
@@ -23,13 +23,61 @@ def get_pos_mouse(position):
     
     return row, col
 
+def main_menu():
+    pygame.display.set_caption("Main Menu")
+    button_face = pygame.image.load("button.jpg")
+    button_face = pygame.transform.scale(button_face, (400, 150))
+    run = True 
+    clock = pygame.time.Clock()
+    WIN.fill("black")
+    
+    while run: 
+        clock.tick(FPS)
+        board = Board()
+        menu_mouse_pos = pygame.mouse.get_pos()
+        
+        play_button = Button(button_face, pos=(400, 150), text_input = "PLAY", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
+        mode_button = Button(button_face, pos=(400, 300), text_input = "GAME MODE", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
+        options_button = Button(button_face, pos=(400, 450), text_input = "OPTIONS", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
+        exit_button = Button(button_face, pos=(400, 600), text_input = "EXIT", font=pygame.font.SysFont("couriernew", 50), base_color="black", hovering_color="red")
+        
+        for selection in [play_button, mode_button, options_button, exit_button]:
+            selection.color_change(menu_mouse_pos)
+            selection.update(WIN)
+            
+        if board.winner() != None: 
+            print(board.winner())
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_button.user_input(menu_mouse_pos):
+                    main()
+                if mode_button.user_input(menu_mouse_pos):
+                    game_mode()
+                if options_button.user_input(menu_mouse_pos):
+                    options()
+                if exit_button.user_input(menu_mouse_pos):
+                    run = False
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+        
 def main():
+    pygame.display.set_caption ('Checkers - Game')
     run = True 
     clock = pygame.time.Clock()
     game = Game(WIN)
     board = Board()
-    while run : 
+    
+    while run: 
         clock.tick(FPS)
+        WIN.fill("black")
         
         if board.winner() != None: 
             print(board.winner())
@@ -37,14 +85,65 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+                pygame.quit()
+                sys.exit()
             
             if event.type == pygame.MOUSEBUTTONDOWN: 
                 pos = pygame.mouse.get_pos()
                 row, col = get_pos_mouse(pos)
                 game.select(row,col)
-                
+        
         game.update()
     
     pygame.quit()
 
-main()
+def game_mode():
+    pygame.display.set_caption('Game Mode Selection')
+    run = True 
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("couriernew", 50)
+    
+    while run: 
+        clock.tick(FPS)
+        WIN.fill("black")
+        mode_text = font.render("This Screen Isn't Finished", True, "white")
+        mode_rect = mode_text.get_rect(center=(400, 400))
+        WIN.blit(mode_text, mode_rect)
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+        
+        ## Need to add a back button to go to main menu
+        #    if event.type == pygame.MOUSEBUTTONDOWN:
+        
+        pygame.display.update()
+
+def options():
+    pygame.display.set_caption('Adjust Game Settings')
+    run = True 
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("couriernew", 50)
+    
+    while run: 
+        clock.tick(FPS)
+        WIN.fill("black")
+        options_text = font.render("This Screen Isn't Finished", True, "white")
+        options_rect = options_text.get_rect(center=(400, 400))
+        WIN.blit(options_text, options_rect)
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                sys.exit()
+        
+        ## Need to add a back button to go to main menu
+        #    if event.type == pygame.MOUSEBUTTONDOWN:
+            
+        pygame.display.update()
+##############################################################################
+main_menu()
+# main()
